8f7ef321e5561ba9538d2b2f6794977593e1ead4
NazarovIlya <eliakohai@gmail.com>
2022-10-26 22:17:00 +0300

    the first task on CODEWARS

---
+++ CS_Training/CS00000/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Kata_V1
+{
+    public static string Smash(string[] words)
+    {
+        string new_str = string.Empty;
+        for (int i = 0; i < words.Length; i++)
+        {
+            new_str += words[i];
+            if (i != words.Length - 1 && words.Length != 1)
+                new_str += " ";
+        }
+        return new_str;
+    }
+}
+
+//  HOWEVER!!!
+// SIPLY
+
+public class Kata_V2
+{
+    public static string Smash(string[] words) => String.Join(" ", words);
+}
